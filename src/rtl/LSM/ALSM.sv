5b44de439bbda5429a340de07f13249541754071
alisakr-git <ali4dsakr@gmail.com>
2026-02-20 00:54:49 +0200

    handled registered signals

--- src/rtl/LSM/ALSM.sv
+++ src/rtl/LSM/ALSM.sv
@@ -1,3 +1,4 @@
+// all lp_state_req encodings
 typedef enum logic [3:0] { 
     Req_NOP       = 'b0000,
     Req_Active    = 'b0001,
@@ -8,6 +9,7 @@ typedef enum logic [3:0] {
     Req_Disable   = 'b1100
 } state_req;
 
+// all pl_sts encodings
 typedef enum logic [3:0] { 
     LL_Reset        = 'b0000,
     LL_Active       = 'b0001,
@@ -20,6 +22,22 @@ typedef enum logic [3:0] {
     LL_Disable      = 'b1100
 } ll_state;
 
+// all valid sb message encodings
+typedef enum { 
+    SB_Req_Active,
+    SB_Req_L1,
+    SB_Req_L2,
+    SB_Req_LinkReset,
+    SB_Req_Disable,
+    SB_Rsp_Active,
+    SB_Rsp_L1,
+    SB_Rsp_L2,
+    SB_Rsp_LinkReset,
+    SB_Rsp_Disable,
+    SB_Rsp_PMNAK
+ } SB_state;
+
+// Internal Main state encodings
 typedef enum  { 
     Reset,
     Active,
@@ -31,17 +49,19 @@ typedef enum  {
     Disable
 } Main_State;
 
+// Reset sub state encodings
 typedef enum {
-    Reset_sub_state,
-    Param_exch,
-    Active_Entry,
-    SB_Active_Req,
-    Active_Req_Await,
-    rx_active_1,
-    SB_rsp_recieved,
-    rx_active_2,
-    Await_FDI_Active
+    Reset_SS,
+    Param_exch_SS,
+    Active_Entry_SS,
+    SB_Active_Req_SS,
+    Active_Req_Await_SS,
+    rx_active_1_SS,
+    SB_rsp_recieved_SS,
+    rx_active_2_SS,
+    Await_FDI_Active_SS
 } Reset_SUB_STATE;
+
 
 module ALSM (
     input logic       clk,
@@ -67,27 +87,31 @@ module ALSM (
     input logic       i_fdi_lp_linkerror,
     input logic       i_fdi_lp_rx_active_sts,
     input logic       i_fdi_lp_stall_ack,
+    input logic       i_fdi_lp_clk_ack,
+    input logic       i_fdi_lp_wake_req,
 
     // FDI outputs
-    input logic       o_fdi_pl_stallreq,
-    input logic       o_fdi_pl_phyinrecenter,
-    input logic       o_fdi_pl_phyinl1,
-    input logic       o_fdi_pl_phyinl2,
-    input logic [2:0] o_fdi_pl_speedmode,
-    input logic       o_fdi_pl_max_speedmode,
-    input logic [2:0] o_fdi_pl_lnk_cfg,
-    input ll_state    o_fdi_pl_state_sts,
-    input logic       o_fdi_pl_inband_pres,
-    input logic       o_fdi_pl_rx_active_req,
+    output logic       o_fdi_pl_stallreq,
+    output logic       o_fdi_pl_phyinrecenter,
+    output logic       o_fdi_pl_phyinl1,
+    output logic       o_fdi_pl_phyinl2,
+    output logic [2:0] o_fdi_pl_speedmode,
+    output logic       o_fdi_pl_max_speedmode,
+    output logic [2:0] o_fdi_pl_lnk_cfg,
+    output ll_state    o_fdi_pl_state_sts,
+    output logic       o_fdi_pl_inband_pres,
+    output logic       o_fdi_pl_rx_active_req,
+    output logic       o_fdi_pl_clk_req,
+    output logic       o_fdi_pl_wake_ack,
 
     // SB inputs
-    input ll_state     i_sb_state_rx,
+    input SB_state     i_sb_state_rx,
     input logic        i_sb_param_exch_done,
 
     // SB outputs
     output logic       o_sb_start_param_exch,
     output logic       o_sb_msg_request,
-    output ll_state    o_sb_state_tx,
+    output SB_state    o_sb_state_tx,
 
     // MB inputs
     input logic        i_MB_retry_clean_boundary_done,
@@ -119,52 +143,120 @@ module ALSM (
     // current sub state, next sub state
     logic [3:0] css, nss;
 
-    logic Protocol_Active, SB_Active;
+    // internal signal definitions
+    logic Protocol_Active;
+    logic sb_active_req_received, sb_active_rsp_received;
 
+    // always request the FDI and RDI to be ungated
+    assign o_rdi_lp_wake_req = 'b1;
+    assign fdi_pl_clk_req = 'b1;
+
+    // next state
     always_comb begin
-
         nms = cms;
         nss = css;
 
-        case (css)
-            Reset_sub_state: 
-                if (i_rdi_pl_state_sts == LL_Active) begin
-                    nss = Param_exch;
+        case (cms)
+            Reset: 
+            case (css)
+                Reset_SS: 
+                    if (i_rdi_pl_state_sts == LL_Active) begin
+                        nss = Param_exch_SS;
+                    end
+                    else begin
+                        nss = Reset_SS;
+                    end
+                Param_exch_SS: 
+                    if (i_sb_param_exch_done) begin
+                        nss = Active_Entry_SS;
+                    end
+                    else begin
+                        nss = Param_exch_SS;
+                    end
+                Active_Entry_SS:
+                    if (Protocol_Active) begin
+                        nss = SB_Active_Req_SS;
+                    end
+                    else if (sb_active_req_received && o_rdi_lp_clk_ack) begin
+                        nss = rx_active_2_SS;
+                    end
+                    else begin
+                        nss = Active_Entry_SS;
+                    end
+                SB_Active_Req_SS:
+                    if (sb_active_rsp_received) begin
+                        nss = Active_Req_Await_SS;
+                    end 
+                    else if (sb_active_req_received) begin
+                        nss = rx_active_1_SS;
+                    end
+                    else begin
+                        nss = SB_Active_Req_SS;
+                    end
+                Active_Req_Await_SS:
+                    if (sb_active_req_received) begin
+                        nss = rx_active_1_SS;
+                    end
+                    else begin
+                        nss = Active_Req_Await_SS;
+                    end
+                rx_active_1_SS:
+                    if (i_fdi_lp_rx_active_sts && sb_active_rsp_received) begin
+                        nss = 'b0;
+                        nms = Active;
+                    end
+                    else if (i_fdi_lp_rx_active_sts && ~sb_active_rsp_received) begin
+                        nss = SB_rsp_recieved_SS;
+                    end
+                    else begin
+                        nss = rx_active_1_SS;
+                    end
+                rx_active_2_SS:
+                    if (i_fdi_lp_rx_active_sts) begin
+                        nss = Await_FDI_Active_SS;
+                    end
+                    else begin
+                        nss = rx_active_2_SS;
+                    end
+                Await_FDI_Active_SS:
+                    if (i_fdi_lp_state_req == Req_Active) begin
+                        nss = SB_rsp_recieved_SS;
+                    end
+                    else begin
+                        nss = Await_FDI_Active_SS;
+                    end
+                SB_rsp_recieved_SS:
+                    if (sb_active_rsp_received) begin
+                        nss = 'b0;
+                        nms = Active;
+                    end
+                    else begin
+                        nss = SB_rsp_recieved_SS;
+                    end
+                default: begin
+                    nss = 'b0; nms = Reset;
                 end
-                else begin
-                    nss = Reset_sub_state;
-                end
-            Param_exch: 
-                if (i_sb_param_exch_done) begin
-                    nss = Active_Entry;
-                end
-                else begin
-                    nss = Param_exch;
-                end
-            Active_Entry:
-                if (Protocol_Active) begin
-                    nss = SB_Active_Req;
-                end
-                else if (SB_Active) begin
-                    nss = rx_active_2;
-                end
+            endcase
         endcase
     end
 
-    // Protocol_Active and SB_Active only start being taken into account when the State is in 
+    // These signals only start being taken into account when the Main State is in 
     // Reset, otherwise they are zero
     always_ff @(negedge rst_n, posedge clk) begin
         if (~rst_n) begin
             Protocol_Active <= 'b0;
-            SB_Active <= 'b0;
+            sb_active_req_received <= 'b0;
+            sb_active_rsp_received <= 'b0;
         end
         else if (cms == Reset) begin
             Protocol_Active <= Protocol_Active | (i_fdi_lp_state_req == Req_Active);
-            SB_Active <= SB_Active | (i_sb_state_rx == LL_Active);
+            sb_active_req_received <= sb_active_req_received | (i_sb_state_rx == SB_Req_Active);
+            sb_active_rsp_received <= sb_active_rsp_received | (i_sb_state_rx == SB_Rsp_Active);
         end
         else begin
             Protocol_Active <= 'b0;
-            SB_Active <= 'b0;
+            sb_active_req_received <= 'b0;
+            sb_active_rsp_received <= 'b0;
         end
     end
 
@@ -176,6 +268,28 @@ module ALSM (
         else begin
             cms <= nms;
             css <= nss;
+        end
+    end
+
+    // Registered signals
+    always_ff @(negedge rst_n, posedge clk) begin
+        if (~rst_n) begin
+            o_fdi_pl_phyinrecenter <= 'b0; 
+            o_fdi_pl_speedmode <= 'b0;
+            o_fdi_pl_lnk_cfg <= 'b0;
+            o_fdi_pl_phyinl1 <= 'b0;
+            o_fdi_pl_phyinl2 <= 'b0;
+            o_fdi_pl_wake_ack <= 'b0;
+            o_rdi_lp_clk_ack <= 'b0;
+        end
+        else begin
+           o_fdi_pl_phyinrecenter <= i_rdi_pl_phyinrecenter; 
+           o_fdi_pl_speedmode <= i_rdi_pl_speedmode;
+           o_fdi_pl_lnk_cfg <= i_rdi_pl_lnk_cfg;
+           o_fdi_pl_phyinl1 <= (i_rdi_pl_state_sts == LL_L1);
+           o_fdi_pl_phyinl2 <= (i_rdi_pl_state_sts == LL_L2);
+           o_fdi_pl_wake_ack <= i_fdi_lp_wake_req;
+           o_rdi_lp_clk_ack <= i_rdi_pl_clk_req;
         end
     end
 endmodule
