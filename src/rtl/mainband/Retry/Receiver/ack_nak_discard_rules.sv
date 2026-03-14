5f6f9d041e79a7df2bbb3735870480af00d8014d
Fatma Fawzy <110131601+Mikaliaju@users.noreply.github.com>
2026-03-14 08:10:14 +0200

    Add files via upload

--- src/rtl/mainband/Retry/Receiver/ack_nak_discard_rules.sv
+++ src/rtl/mainband/Retry/Receiver/ack_nak_discard_rules.sv
@@ -4,87 +4,87 @@
 import common_pkg::*;
 
 module ack_nak_discard_rules (
-    input  logic       clk, rst_n,
-    input  logic       init, //software reset
-    input  logic       i_crc_error, // crc error in the received flit from mainband receiver
-    input  phase_t     i_phase, // phase of the link 
-    input  logic       i_nop_payload_flit, // 1 : nop, 0 : payload
-    input  replay_command_t i_replay_command, // ack or nak or explicit
-    input  logic [7:0] i_seq_num, // sequence number of the received flit
-    input  logic [7:0] i_implicit_rx_flit_seq_num, // implicit sequence number from implicit seq number module (new).
-    input  logic [7:0] i_next_expect_rx_flit_seq_num, //from counter tracker
-    input  logic [7:0] i_tx_acknak_flit_seq_num, //from counter tracker
-    output logic       o_log_uie, //log uncorrectable internal error in register file
-    output logic       o_discard_flit, //discard flit
-    output logic       o_discard_payload, //discard payload
-    output logic       o_nak_scheduled, //nak scheduled
-    output logic       o_nak_schedule_type, //nak schedule type
-    output logic [7:0] o_tx_acknak_flit_seq_num, //tx acknak flit to counter tracker
-    output logic [7:0] o_next_expect_rx_flit_seq_num //next expect rx flit to counter tracker
+    input  logic            clk, rst_n,
+    input  logic            init,                          // software reset
+    input  logic            i_crc_error,                  // crc error in the received flit from mainband receiver
+    input  phase_t          i_phase,                      // phase of the link
+    input  logic            i_nop_payload_flit,           // 1 : nop, 0 : payload
+    input  replay_command_t i_replay_command,              // ack or nak or explicit
+    input  logic [7:0]      i_seq_num,                    // sequence number of the received flit
+    input  logic [7:0]      i_implicit_rx_flit_seq_num,   // implicit sequence number from implicit seq number module (new)
+    input  logic [7:0]      i_next_expect_rx_flit_seq_num, // from counter tracker
+    input  logic [7:0]      i_tx_acknak_flit_seq_num,     // from counter tracker
+    output logic            o_log_uie,                    // log uncorrectable internal error in register file
+    output logic            o_discard_flit,               // discard flit
+    output logic            o_discard_payload,            // discard payload
+    output logic            o_nak_scheduled,              // nak scheduled
+    output logic            o_nak_schedule_type,          // nak schedule type
+    output logic [7:0]      o_tx_acknak_flit_seq_num,     // tx acknak flit to counter tracker
+    output logic [7:0]      o_next_expect_rx_flit_seq_num // next expect rx flit to counter tracker
 );
 
-logic r_crc_error_d;
-logic r_nop_payload_flit_d;
-logic [7:0] r_seq_num_d; 
+logic            r_crc_error_d;
+logic            r_nop_payload_flit_d;
+logic [7:0]      r_seq_num_d;
 replay_command_t r_replay_command_d;
 
-//delay by 1 cycle to wait for implicit rx seq num to be updated
+// delay by 1 cycle to wait for implicit rx seq num to be updated
 always_ff @ (posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        r_crc_error_d <= 0;
+    if (!rst_n) begin
+        r_crc_error_d        <= 0;
         r_nop_payload_flit_d <= 0;
-        r_seq_num_d <= 0;
-        r_replay_command_d <= explicit;
+        r_seq_num_d          <= 0;
+        r_replay_command_d   <= explicit;
     end
-    else if(!init) begin
-        r_crc_error_d <= 0;
+    else if (!init) begin
+        r_crc_error_d        <= 0;
         r_nop_payload_flit_d <= 0;
-        r_seq_num_d <= 0;
-        r_replay_command_d <= explicit;
+        r_seq_num_d          <= 0;
+        r_replay_command_d   <= explicit;
     end
     else begin
-        r_crc_error_d <= i_crc_error;
+        r_crc_error_d        <= i_crc_error;
         r_nop_payload_flit_d <= i_nop_payload_flit;
-        r_seq_num_d <= i_seq_num;
-        r_replay_command_d <= i_replay_command;
+        r_seq_num_d          <= i_seq_num;
+        r_replay_command_d   <= i_replay_command;
     end
 end
 
 always_ff @ (posedge clk or negedge rst_n) begin
-    if(!rst_n) begin
-        o_log_uie <= 1'b0;
-        o_discard_flit <= 1'b0;
-        o_discard_payload <= 1'b0;
-        o_nak_scheduled <= 1'b0;
-        o_nak_schedule_type <= 1'b0;
-        o_tx_acknak_flit_seq_num <= 8'b0;
+    if (!rst_n) begin
+        o_log_uie                     <= 1'b0;
+        o_discard_flit                <= 1'b0;
+        o_discard_payload             <= 1'b0;
+        o_nak_scheduled               <= 1'b0;
+        o_nak_schedule_type           <= 1'b0;
+        o_tx_acknak_flit_seq_num      <= 8'b0;
         o_next_expect_rx_flit_seq_num <= 8'b0;
     end
-    else if(!init) begin
-        o_log_uie <= 1'b0;
-        o_discard_flit <= 1'b0;
-        o_discard_payload <= 1'b0;
-        o_nak_scheduled <= 1'b0;
-        o_nak_schedule_type <= 1'b0;
-        o_tx_acknak_flit_seq_num <= 8'b0;
+    else if (!init) begin
+        o_log_uie                     <= 1'b0;
+        o_discard_flit                <= 1'b0;
+        o_discard_payload             <= 1'b0;
+        o_nak_scheduled               <= 1'b0;
+        o_nak_schedule_type           <= 1'b0;
+        o_tx_acknak_flit_seq_num      <= 8'b0;
         o_next_expect_rx_flit_seq_num <= 8'b0;
-    end 
+    end
     else begin
-        if(r_crc_error_d) begin
-            if(o_nak_scheduled) begin
+        if (r_crc_error_d) begin
+            if (o_nak_scheduled) begin
                 flit_discard_1();
             end
-            else begin 
+            else begin
                 nak_schedule_0();
             end
         end
-        else if(r_replay_command_d == explicit 
+        else if (r_replay_command_d == explicit
             && i_phase == normal_exchange && r_seq_num_d == 8'b0) begin
             flit_discard_2();
         end
         else begin
-            if(!r_nop_payload_flit_d) begin
-                if(bad_nop_sequence_number()) begin
+            if (!r_nop_payload_flit_d) begin
+                if (bad_nop_sequence_number()) begin
                     nak_schedule_2();
                 end
                 else begin
@@ -92,13 +92,13 @@ always_ff @ (posedge clk or negedge rst_n) begin
                 end
             end
             else begin
-                if(o_nak_scheduled) begin
+                if (o_nak_scheduled) begin
                     standard_nak_procedure();
                 end
                 else begin
-                    if(duplicate_sequence_number()) flit_discard_0();
-                    else if(bad_sequence_number()) nak_schedule_2();
-                    else ack_schedule_0();
+                    if      (duplicate_sequence_number()) flit_discard_0();
+                    else if (bad_sequence_number())       nak_schedule_2();
+                    else                                  ack_schedule_0();
                 end
             end
         end
@@ -127,51 +127,57 @@ task standard_nak_procedure();
         end
     end
     else if ((r_replay_command_d == explicit) &&
-        (i_implicit_rx_flit_seq_num == i_next_expect_rx_flit_seq_num)) begin
-		    ack_schedule_1();
-	    end
-	    else begin
-		    if (o_nak_scheduled) begin
-		        flit_discard_0();
-		    end
-		    else begin
-		        nak_schedule_2();
-		end
+             (i_implicit_rx_flit_seq_num == i_next_expect_rx_flit_seq_num)) begin
+        ack_schedule_1();
+    end
+    else begin
+        if (o_nak_scheduled) begin
+            flit_discard_0();
+        end
+        else begin
+            nak_schedule_2();
+        end
     end
 endtask
 
-    task automatic ack_schedule_0();
-        o_tx_acknak_flit_seq_num <= i_next_expect_rx_flit_seq_num;
-        o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num + 1;
-    endtask
-    task automatic ack_schedule_1();
-        o_tx_acknak_flit_seq_num <= i_implicit_rx_flit_seq_num;
-        o_next_expect_rx_flit_seq_num <= i_implicit_rx_flit_seq_num - 1;
-    endtask
-    task automatic nak_schedule_0();
-        o_discard_flit <= 1'b1;
-        o_nak_scheduled <= 1'b1;
-        o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
-    endtask
-    task automatic nak_schedule_2();
-        o_discard_payload <= 1'b1;
-        o_tx_acknak_flit_seq_num <= i_next_expect_rx_flit_seq_num - 1;
-        o_nak_scheduled <= 1'b1;
-        o_nak_schedule_type <= standard_nak;
-        o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
-    endtask 
-    task automatic flit_discard_0();
-        o_discard_payload <= 1'b1;
-        o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
-    endtask
-    task automatic flit_discard_1();
-        o_discard_flit <= 1'b1;
-        o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
-    endtask
-    task automatic flit_discard_2();
-        o_discard_flit <= 1'b1;
-        o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
-        o_log_uie <= 1'b1;
-    endtask 
-    
+task automatic ack_schedule_0();
+    o_tx_acknak_flit_seq_num      <= i_next_expect_rx_flit_seq_num;
+    o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num + 1;
+endtask
+
+task automatic ack_schedule_1();
+    o_tx_acknak_flit_seq_num      <= i_implicit_rx_flit_seq_num;
+    o_next_expect_rx_flit_seq_num <= i_implicit_rx_flit_seq_num - 1;
+endtask
+
+task automatic nak_schedule_0();
+    o_discard_flit                <= 1'b1;
+    o_nak_scheduled               <= 1'b1;
+    o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
+endtask
+
+task automatic nak_schedule_2();
+    o_discard_payload             <= 1'b1;
+    o_tx_acknak_flit_seq_num      <= i_next_expect_rx_flit_seq_num - 1;
+    o_nak_scheduled               <= 1'b1;
+    o_nak_schedule_type           <= standard_nak;
+    o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
+endtask
+
+task automatic flit_discard_0();
+    o_discard_payload             <= 1'b1;
+    o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
+endtask
+
+task automatic flit_discard_1();
+    o_discard_flit                <= 1'b1;
+    o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
+endtask
+
+task automatic flit_discard_2();
+    o_discard_flit                <= 1'b1;
+    o_next_expect_rx_flit_seq_num <= i_next_expect_rx_flit_seq_num;
+    o_log_uie                     <= 1'b1;
+endtask
+
 endmodule
