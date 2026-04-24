bf844bd1a2f20209f19d3fb06455e57109f25dd4
alinourelaziz124-hash <alinourelaziz124@gmail.com>
2026-04-24 21:45:26 +0300

    Update UC_MB_Mainband_wrap.sv

--- src/rtl/TOP/UC_MB_Mainband_wrap.sv
+++ src/rtl/TOP/UC_MB_Mainband_wrap.sv
@@ -99,53 +99,53 @@ module UC_MB_Mainband_wrap #(
     //----------------------------------------------------------
     UC_MB_Mainband #(
         .DATA_PATH ( DATA_PATH ),
-        .DLLP      ( DLLP      )
+        .DLLP      ( DLLP )
     ) u_UC_MB_Mainband (
-        .i_clk                  ( i_clk             ),
-        .i_rst_n                ( i_rst_n            ),
-        .i_init                 ( i_init             ),
+        .i_clk                  ( i_clk ),
+        .i_rst_n                ( i_rst_n ),
+        .i_init                 ( i_init ),
 
         // FDI TX
-        .i_lp_irdy_fdi          ( w_lp_irdy_fdi     ),
-        .i_lp_valid_fdi         ( w_lp_valid_fdi     ),
-        .i_lp_data_fdi          ( w_lp_data_fdi      ),
-        .i_lp_dllp              ( w_lp_dllp          ),
-        .i_lp_dllp_valid        ( w_lp_dllp_valid    ),
-        .i_lp_dllp_ofc          ( w_lp_dllp_ofc      ),
-        .i_lp_stream            ( w_lp_stream        ),
-        .o_pl_trdy_fdi          ( w_pl_trdy_fdi      ),
+        .i_lp_irdy_fdi          ( w_lp_irdy_fdi ),
+        .i_lp_valid_fdi         ( w_lp_valid_fdi ),
+        .i_lp_data_fdi          ( w_lp_data_fdi ),
+        .i_lp_dllp              ( w_lp_dllp ),
+        .i_lp_dllp_valid        ( w_lp_dllp_valid ),
+        .i_lp_dllp_ofc          ( w_lp_dllp_ofc ),
+        .i_lp_stream            ( w_lp_stream ),
+        .o_pl_trdy_fdi          ( w_pl_trdy_fdi ),
 
         // FDI RX
-        .o_pl_data_fdi          ( w_pl_data_fdi      ),
-        .o_pl_valid_fdi         ( w_pl_valid_fdi     ),
-        .o_pl_stream            ( w_pl_stream        ),
-        .o_pl_dllp              ( w_pl_dllp          ),
-        .o_pl_dllp_valid        ( w_pl_dllp_valid    ),
-        .o_pl_dllp_ofc          ( w_pl_dllp_ofc      ),
-        .o_flit_cancel          ( w_flit_cancel      ),
+        .o_pl_data_fdi          ( w_pl_data_fdi ),
+        .o_pl_valid_fdi         ( w_pl_valid_fdi ),
+        .o_pl_stream            ( w_pl_stream ),
+        .o_pl_dllp              ( w_pl_dllp ),
+        .o_pl_dllp_valid        ( w_pl_dllp_valid ),
+        .o_pl_dllp_ofc          ( w_pl_dllp_ofc ),
+        .o_flit_cancel          ( w_flit_cancel ),
 
         // RDI TX
-        .i_pl_trdy              ( w_pl_trdy          ),
-        .o_lp_data_rdi          ( w_lp_data_rdi      ),
-        .o_lp_valid_rdi         ( w_lp_valid_rdi     ),
-        .o_lp_irdy_rdi          ( w_lp_irdy_rdi      ),
+        .i_pl_trdy              ( w_pl_trdy ),
+        .o_lp_data_rdi          ( w_lp_data_rdi ),
+        .o_lp_valid_rdi         ( w_lp_valid_rdi ),
+        .o_lp_irdy_rdi          ( w_lp_irdy_rdi ),
 
         // RDI RX
-        .i_pl_data_rdi          ( w_pl_data_rdi      ),
-        .i_pl_valid_rdi         ( w_pl_valid_rdi     ),
+        .i_pl_data_rdi          ( w_pl_data_rdi ),
+        .i_pl_valid_rdi         ( w_pl_valid_rdi ),
 
         // LSM Packer
-        .i_packer_en            ( i_packer_en        ),
-        .i_flit_boundary        ( i_flit_boundary    ),
-        .i_flush                ( i_flush            ),
-        .i_drain                ( i_drain            ),
-        .o_flit_boundary_done   ( o_flit_boundary_done),
-        .o_flush_done           ( o_flush_done       ),
-        .o_drain_done           ( o_drain_done       ),
+        .i_packer_en            ( i_packer_en ),
+        .i_flit_boundary        ( i_flit_boundary ),
+        .i_flush                ( i_flush ),
+        .i_drain                ( i_drain ),
+        .o_flit_boundary_done   ( o_flit_boundary_done ),
+        .o_flush_done           ( o_flush_done ),
+        .o_drain_done           ( o_drain_done ),
 
         // LSM Unpacker
-        .i_unpacker_en          ( i_unpacker_en      ),
-        .i_stop_stream          ( i_stop_stream      )
+        .i_unpacker_en          ( i_unpacker_en ),
+        .i_stop_stream          ( i_stop_stream )
     );
 
 endmodule : UC_MB_Mainband_wrap
