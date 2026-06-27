b49251253d8ff39cc4cec43c23e91bb78f45ec0f
Core_Kingdom <2972880695@qq.com>
2026-06-27 08:45:00 +0800

    ran spyglass on the entire system (retry included)

--- spyglass/TOP/spyglass.tcl
+++ spyglass/TOP/spyglass.tcl
@@ -1,11 +1,12 @@
+set_option incdir "../../src/rtl/common"
 set_option enableSV yes
 read_file -type verilog [exec cat sources.list]
 set_option top UC_TOP
 
 set_option mthresh 40000
-current_goal Design_Read -top UC_TOP
+current_goal Design_Read -top UC_TOP_EP
 
-current_goal lint/lint_rtl -top UC_TOP
+current_goal lint/lint_rtl -top UC_TOP_EP
 run_goal
 
 quit
